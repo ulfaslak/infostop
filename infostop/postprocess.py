df83fa2338d412551e16f123a7e3a5d5667bd066
Ulf Aslak Jensen <ulasje@heisenberg.imm.dtu.dk>
2020-11-10 20:36:54 +0100

    change compute_intervals so the output does not consider the lat,lon of stop locations (this is redundant and should be handled by the user)

--- infostop/postprocess.py
+++ infostop/postprocess.py
@@ -1,66 +1,48 @@
 import numpy as np
 
-def compute_intervals(coords, coord_labels, max_time_between=86400, distance_metric="haversine"):
+def compute_intervals(labels, times, max_time_between=86400):
     """Compute stop and moves intervals from the list of labels.
     
     Parameters
     ----------
-        coords : np.array (shape=(N, 2) or shape=(N,3))
-        coord_labels: 1d np.array of integers
-
+        labels: 1d np.array of integers
+        times: 1d np.array of integers. `len(labels) == len(times)`.
     Returns
     -------
-        intervals : array-like (shape=(N_intervals, 5))
-            Columns are "label", "start_time", "end_time", "latitude", "longitude"
+        intervals : array-like (shape=(N_intervals, 3))
+            Columns are "label", "start_time", "end_time"
     
     """
+    assert len(labels) == len(times), '`labels` and `times` must match in length'
     
-    if coords.shape[1] == 2:
-        times = np.array(list(range(0, len(coords))))
-        coords = np.hstack([coords, times.reshape(-1,1)])
-        
-    trajectory = np.hstack([coords, coord_labels.reshape(-1,1)])
-    
+    # Input and output sequence
+    trajectory = np.hstack([labels.reshape(-1, 1), times.reshape(-1,1)])
     final_trajectory = []
     
-    #initialize values
-    lat_prec, lon_prec, t_start, loc_prec = trajectory[0]  
-    t_end = t_start 
-    median_lat = [lat_prec]
-    median_lon = [lon_prec]
+    # Init values
+    loc_prev, t_start = trajectory[0]  
+    t_end = t_start
 
-    #Loop through trajectory
-    for lat, lon, time, loc in trajectory[1:]:
+    # Loop through trajectory
+    for loc, time in trajectory[1:]:
         
-        #if the location name has not changed update the end of the interval
-        if (loc==loc_prec) and (time-t_end)<(max_time_between):
+        # if the location name has not changed update the end of the interval
+        if (loc == loc_prev) and (time - t_end) < max_time_between:
             t_end = time
-            median_lat.append(lat)
-            median_lon.append(lon)
-            
-            
-        #if the location name has changed build the interval and reset values
+              
+        # if the location name has changed build the interval
         else:
-            if loc_prec==-1:
-                final_trajectory.append([loc_prec, t_start,  t_end, np.nan, np.nan])
-            else:
-                final_trajectory.append([loc_prec, t_start,  t_end, np.median(median_lat), np.median(median_lon)])
-                
+            final_trajectory.append([loc_prev, t_start,  t_end])
+            
+            # and reset values
             t_start = time 
             t_end = time 
-            median_lat = []
-            median_lon = []
-            
         
-        #update current values
-        loc_prec = loc
-        lat_prec = lat
-        lon_prec = lon
+        # update current values
+        loc_prev = loc
         
-    #Add last group
-    if loc_prec==-1:
-        final_trajectory.append([loc_prec, t_start,  t_end, np.nan, np.nan])
-    else:
-        final_trajectory.append([loc_prec, t_start,  t_end, np.median(median_lat), np.median(median_lon)])
+    # Add last group
+    if loc_prev == -1:
+        final_trajectory.append([loc_prev, t_start,  t_end])
 
     return final_trajectory
