604356c18e2c25408e252af68d03bbef681bc845
Ulf Aslak <ulfjensen@gmail.com>
2022-02-20 21:13:16 +0100

    Make the example function more example-like

--- tests/test_models.py
+++ tests/test_models.py
@@ -6,11 +6,11 @@ import numpy as np
 # Example test function. Run `make test` to see that it passes.`
 def test_example():
     # Function
-    func = _.area
+    func = lambda x: f"I'm an example {x}"
 
     # Params and returns
-    params = dict(polygon=[[1, 0], [0, 0], [0, 1], [1, 1]])
-    returns = 1
+    params = dict(x="function")
+    returns = "I'm an example function"
 
     # Test
     assert func(**params) == returns
