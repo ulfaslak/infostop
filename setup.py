8df4e55159d9880960e254a6dd280071f24d40e9
Ulf Aslak <ulfjensen@gmail.com>
2022-02-20 20:29:00 +0100

    change required infomap version to exactly 1.0.6

--- setup.py
+++ setup.py
@@ -7,40 +7,45 @@ with open("README.md", "r") as fh:
 
 exec(open("./infostop/metadata.py").read())
 
+
 class get_pybind_include(object):
     """Helper class to determine the pybind11 include path
     The purpose of this class is to postpone importing pybind11
     until it is actually installed, so that the ``get_include()``
-    method can be invoked. """
+    method can be invoked."""
 
     def __init__(self, user=False):
         self.user = user
 
     def __str__(self):
         import pybind11
+
         return pybind11.get_include(self.user)
+
 
 ext_modules = [
     Extension(
-        'cpputils',
-        ['cpputils/main.cpp'],
+        "cpputils",
+        ["cpputils/main.cpp"],
         include_dirs=[
             # Path to pybind11 headers
             get_pybind_include(),
             get_pybind_include(user=True),
-            "./cpputils/"
+            "./cpputils/",
         ],
-        language='c++'
+        language="c++",
     ),
 ]
+
 
 def has_flag(compiler, flagname):
     """Return a boolean indicating whether a flag name is supported on
     the specified compiler.
     """
     import tempfile
-    with tempfile.NamedTemporaryFile('w', suffix='.cpp') as f:
-        f.write('int main (int argc, char **argv) { return 0; }')
+
+    with tempfile.NamedTemporaryFile("w", suffix=".cpp") as f:
+        f.write("int main (int argc, char **argv) { return 0; }")
         try:
             compiler.compile([f.name], extra_postargs=[flagname])
         except setuptools.distutils.errors.CompileError:
@@ -52,45 +57,48 @@ def cpp_flag(compiler):
     """Return the -std=c++[11/14/17] compiler flag.
     The newer version is prefered over c++11 (when it is available).
     """
-    flags = ['-std=c++14', '-std=c++11']
+    flags = ["-std=c++14", "-std=c++11"]
 
     for flag in flags:
-        if has_flag(compiler, flag): return flag
+        if has_flag(compiler, flag):
+            return flag
 
-    raise RuntimeError('Unsupported compiler -- at least C++11 support '
-                       'is needed!')
-    
+    raise RuntimeError("Unsupported compiler -- at least C++11 support " "is needed!")
+
+
 class BuildExt(build_ext):
     """A custom build extension for adding compiler-specific options."""
+
     c_opts = {
-        'msvc': ['/EHsc'],
-        'unix': [],
+        "msvc": ["/EHsc"],
+        "unix": [],
     }
     l_opts = {
-        'msvc': [],
-        'unix': [],
+        "msvc": [],
+        "unix": [],
     }
 
-    if sys.platform == 'darwin':
-        darwin_opts = ['-stdlib=libc++', '-mmacosx-version-min=10.7']
-        c_opts['unix'] += darwin_opts
-        l_opts['unix'] += darwin_opts
+    if sys.platform == "darwin":
+        darwin_opts = ["-stdlib=libc++", "-mmacosx-version-min=10.7"]
+        c_opts["unix"] += darwin_opts
+        l_opts["unix"] += darwin_opts
 
     def build_extensions(self):
         ct = self.compiler.compiler_type
         opts = self.c_opts.get(ct, [])
         link_opts = self.l_opts.get(ct, [])
-        if ct == 'unix':
+        if ct == "unix":
             opts.append('-DVERSION_INFO="%s"' % self.distribution.get_version())
             opts.append(cpp_flag(self.compiler))
-            if has_flag(self.compiler, '-fvisibility=hidden'):
-                opts.append('-fvisibility=hidden')
-        elif ct == 'msvc':
+            if has_flag(self.compiler, "-fvisibility=hidden"):
+                opts.append("-fvisibility=hidden")
+        elif ct == "msvc":
             opts.append('/DVERSION_INFO=\\"%s\\"' % self.distribution.get_version())
         for ext in self.extensions:
             ext.extra_compile_args = opts
             ext.extra_link_args = link_opts
         build_ext.build_extensions(self)
+
 
 setup(
     name="infostop",
@@ -103,22 +111,22 @@ setup(
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     ext_modules=ext_modules,
-    setup_requires=['pybind11>=2.4'],
+    setup_requires=["pybind11>=2.4"],
     install_requires=[
-        'pybind11>=2.4',
-        'infomap>=1.0.6',
-        'folium>=0.7.0',
-        'numpy',
-        'tqdm',
-        'scikit-learn',
+        "pybind11>=2.4",
+        "infomap==1.0.6",
+        "folium>=0.7.0",
+        "numpy",
+        "tqdm",
+        "scikit-learn",
     ],
-    cmdclass={'build_ext': BuildExt},
+    cmdclass={"build_ext": BuildExt},
     zip_safe=False,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        'Topic :: Scientific/Engineering :: Information Analysis',
-        'Topic :: Scientific/Engineering :: Mathematics'
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Scientific/Engineering :: Mathematics",
     ],
 )
